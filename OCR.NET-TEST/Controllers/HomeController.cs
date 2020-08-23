c7a94d210b72883adcf2ee25be85b52ba381f309
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-08-23 08:01:03 -0500

    temp commit

--- OCR.NET-TEST/Controllers/HomeController.cs
+++ OCR.NET-TEST/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -32,6 +33,35 @@ namespace OCR.NET_TEST.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Test()
+        {
+
+            var roots = new List<Root>()
+            {
+                new Root()
+                {
+                    log_id = 1,
+                }
+            };
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("类型, 名称, commodity");
+
+            foreach (var root in roots)
+            {
+
+                sb.AppendLine($"{root.log_id}, {root.log_id}");
+
+            }
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+
+            return File(Encoding.GetEncoding("GB2312").GetBytes(sb.ToString()), "text/csv", "invoices.csv");
         }
     }
 }
